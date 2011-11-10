4022ccd89da7097eeedba682148ee9976f3cff36
Andrew Mleczko <andrew@mleczko.net>
2011-11-10 16:40:16 +0100

    fix renderer

--- fa/bootstrap/renderers.py
+++ fa/bootstrap/renderers.py
@@ -3,6 +3,7 @@ from formalchemy import fields
 from formalchemy import helpers as h
 from fa.jquery import renderers
 import fanstatic_resources
+from formalchemy.exceptions import FieldNotFoundError
 
 
 def AutocompleteRelationRenderer(filter_by='id', renderer=fields.IntegerFieldRenderer, **jq_options):
@@ -32,6 +33,12 @@ def AutocompleteRelationRenderer(filter_by='id', renderer=fields.IntegerFieldRen
             children = h.content_tag("span", radio, class_="add-on")
             html = h.content_tag("div", children, class_="input-prepend")
             return ''.join(html)
+
+        def _serialized_value(self):
+            try:
+                return super(Renderer,self)._serialized_value()
+            except FieldNotFoundError:
+                return None
 
     jq_options.update(filter_by=filter_by, show_input=True)
 
