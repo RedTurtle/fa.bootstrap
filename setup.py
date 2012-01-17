38058850f6af3c17e51695cefb805d5ea57764a9
Andrew Mleczko <andrew@mleczko.net>
2012-01-17 14:53:00 +0100

    Back to development: 0.2.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2.4'
+version = '0.2.5.dev0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
