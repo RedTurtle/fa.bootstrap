6d192485e49166bfce2e37b71ce10c86d990ef92
Andrew Mleczko <andrew@mleczko.net>
2012-05-23 18:19:47 +0200

    remove alerts

--- fa/bootstrap/fanstatic_resources.py
+++ fa/bootstrap/fanstatic_resources.py
@@ -7,7 +7,6 @@ from fa.jquery.fanstatic_resources import fa_js
 
 fa_bootstrap_library = Library('fa_bootstrap', 'static')
 listing = Resource(fa_bootstrap_library, 'js/listing.js', depends=[jquery])
-alerts = Resource(fa_bootstrap_library, 'js/alerts.js', depends=[jquery])
 autocomplete = Resource(fa_bootstrap_library, 'js/autocomplete.js', depends=[jqueryui, fa_js])
 
-bootstrap = Group([listing, alerts, bootstrap, tablesorter])
+bootstrap = Group([listing, bootstrap, tablesorter])
--- fa/bootstrap/static/js/alerts.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    window.setTimeout(function() { 
-        $(".alert").children('a').trigger('click');
-    }, 2000);
-});
