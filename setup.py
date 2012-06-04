2fe606fbc4901155c2e32ee93f500a002bebfcd6
Andrew Mleczko <andrew@mleczko.net>
2012-06-04 11:59:41 +0200

    Preparing release 0.3.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3.3.dev0'
+version = '0.3.3'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
