5dc2ad6d20fd7e96b0d39c0cb47eb8184d6a7589
Taha Jahangir <mtjahangir@gmail.com>
2014-04-05 20:17:58 +0430

    Compatible with bootstrap 3 and pyramid 1.5

--- fa/bootstrap/renderers.py
+++ fa/bootstrap/renderers.py
@@ -17,7 +17,7 @@ def AutocompleteRelationRenderer(filter_by='id', renderer=fields.IntegerFieldRen
 
         def update_options(self, options, kwargs):
             kwargs['source'] = self.request.fa_url(
-                    self.field.relation_type().__name__, 'autocomplete')
+                self.field.relation_type().__name__, 'autocomplete')
 
         def render(self, **kwargs):
             fanstatic_resources.autocomplete.need()
@@ -36,7 +36,7 @@ def AutocompleteRelationRenderer(filter_by='id', renderer=fields.IntegerFieldRen
 
         def _serialized_value(self):
             try:
-                return super(Renderer,self)._serialized_value()
+                return super(Renderer, self)._serialized_value()
             except FieldNotFoundError:
                 return None
 
@@ -44,5 +44,41 @@ def AutocompleteRelationRenderer(filter_by='id', renderer=fields.IntegerFieldRen
 
     return renderers.jQueryFieldRenderer('bootstrap_autocomplete_relation', renderer=Renderer, **jq_options)
 
+
 @renderers.alias(AutocompleteRelationRenderer)
 def autocomplete_relation(): pass
+
+
+class BootstrapFieldMixin(object):
+    """
+        Mixin to add `class="form-control"` to renderers
+    """
+    def render(self, **kwargs):
+        if 'class' in kwargs:
+            kwargs['class'] += ' form-control'
+        else:
+            kwargs['class'] = ' form-control'
+        return super(BootstrapFieldMixin, self).render(**kwargs)
+
+
+class BootstrapBooleanFieldRenderer(fields.CheckBoxFieldRenderer):
+    def render(self, **kwargs):
+        checkbox = super(BootstrapBooleanFieldRenderer, self).render(**kwargs)
+        return '<span class="input-group" style="width:1px"><span class="input-group-addon">%s</span>' \
+               '<span class="form-control">%s</span></span>' % (checkbox, 'True')
+
+
+class BootstrapTextFieldRenderer(BootstrapFieldMixin, fields.TextFieldRenderer):
+    pass
+
+
+class BootstrapIntegerFieldRenderer(BootstrapFieldMixin, fields.IntegerFieldRenderer):
+    pass
+
+
+class BootstrapFloatFieldRenderer(BootstrapFieldMixin, fields.FloatFieldRenderer):
+    pass
+
+
+class BootstrapIntervalFieldRenderer(BootstrapFieldMixin, fields.IntervalFieldRenderer):
+    pass
--- fa/bootstrap/scaffolds/__init__.py
+++ fa/bootstrap/scaffolds/__init__.py
@@ -1,6 +1,7 @@
 from paste.util.template import paste_script_template_renderer
 from pyramid.scaffolds import PyramidTemplate
 
+
 class PyramidFormAlchemyBootstrapTemplate(PyramidTemplate):
     _template_dir = ('pyramid_fa_bootstrap')
     summary = "Pyramid SQLAlchemy project using fa.bootstrap"
